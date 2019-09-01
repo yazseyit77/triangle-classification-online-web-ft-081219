058538bc0fde41b15082fa55acf1266358618420
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-01 19:13:58 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,14 +21,14 @@ class Triangle
   end
 
   def kind
-    if valid?
-      if @triangle_sides.uniq.length == 1
-        return :equilateral
-      elsif @triangle_sides.uniq.length == 2
-        return :isosceles
-      else
-        return :scalene
-      end
+      if valid?
+        if @triangle_sides.uniq.length == 1
+          return :equilateral
+        elsif @triangle_sides.uniq.length == 2
+          return :isosceles
+        else
+          return :scalene
+        end
       else
         raise TriangleError
       end
